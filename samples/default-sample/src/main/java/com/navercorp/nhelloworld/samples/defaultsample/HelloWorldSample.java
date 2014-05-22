a08e509dae1dc723a3c9fc7cedd9109f80c949d1
Wonjun Kim <wonjun.kim@navercorp.com>
2014-05-22 10:50:20 +0900

    [Issue-2] Add the default sample application

---
+++ samples/default-sample/src/main/java/com/navercorp/nhelloworld/samples/defaultsample/HelloWorldSample.java
@@ -0,0 +1,11 @@
+package com.navercorp.nhelloworld.samples.defaultsample;
+
+import com.navercorp.nhelloworld.library.helloworld.*;
+
+public class HelloWorldSample {
+
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+		System.out.println(helloWorld.getHello() + helloWorld.getComma() + " " + helloWorld.getWorld());
+	}
+}
